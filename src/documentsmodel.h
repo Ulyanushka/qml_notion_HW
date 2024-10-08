b4798a1fb8b9c2d61f82c718f9123640ec87fd18
Ulyana <ulyana.khanbekova@gmail.com>
2024-10-09 01:07:03 +0300

    new tree model: start

--- src/documentsmodel.h
+++ src/documentsmodel.h
@@ -67,6 +67,34 @@ private:
 };
 
 
+class DocumentsTreeModel : public QAbstractItemModel
+{
+    Q_OBJECT
+    QML_ELEMENT
+
+public:
+    Q_DISABLE_COPY_MOVE(DocumentsTreeModel)
+
+//    explicit TreeModel(const QString &data, QObject *parent = nullptr);
+//    ~TreeModel() override;
+
+//    QVariant data(const QModelIndex &index, int role) const override;
+//    Qt::ItemFlags flags(const QModelIndex &index) const override;
+//    QVariant headerData(int section, Qt::Orientation orientation,
+//                        int role = Qt::DisplayRole) const override;
+//    QModelIndex index(int row, int column,
+//                      const QModelIndex &parent = {}) const override;
+//    QModelIndex parent(const QModelIndex &index) const override;
+//    int rowCount(const QModelIndex &parent = {}) const override;
+//    int columnCount(const QModelIndex &parent = {}) const override;
+
+private:
+//    static void setupModelData(const QList<QStringView> &lines, TreeItem *parent);
+
+//    std::unique_ptr<TreeItem> rootItem;
+};
+
+
 class DocumentsModel : public QAbstractListModel
 {
     Q_OBJECT
